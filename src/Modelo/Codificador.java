a9243ec1c8faace1e7538a13b91d830d9ff1262d
Erick Chaparro <eechaparrom@udistrital.edu.co>
2023-02-19 12:11:59 -0500

    Funcion base64 no terminada

--- src/Modelo/Codificador.java
+++ src/Modelo/Codificador.java
@@ -1,16 +1,24 @@
 package Modelo;
 
+import javax.swing.*;
+
 public class Codificador {
     private String txtAscii;
+    private String[] binaries;
+    private JtableData jt;
 
     public String[] binary(){
         char[] chars = txtAscii.toCharArray(); //la cadena se pasa a un array de chars
-        String[] binaries = new String[chars.length]; //se crea un arreglo con la longitud de los chars
+        binaries = new String[chars.length]; //se crea un arreglo con la longitud de los chars
         for(int i = 0; i < chars.length; i++){
             int caracter = chars[i]; //se pasa el acaracter a entero
             String binary = Integer.toBinaryString(caracter); // de entero a binario
             binaries[i] = binary; //se agregar el binario al array
         }
         return binaries;
+    }
+
+    public void base64(){
+        String acum;
     }
 }
