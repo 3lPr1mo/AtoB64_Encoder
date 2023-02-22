9b174ed6115b793210bd326b221a531788ba2e87
Erick Chaparro <eechaparrom@udistrital.edu.co>
2023-02-21 21:19:45 -0500

    PROYECTO TERMINADO QCHAU

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import Control.Controlador;
 
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Controlador Control = new Controlador();
